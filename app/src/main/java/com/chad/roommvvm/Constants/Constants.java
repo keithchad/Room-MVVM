34aee1175991a41fe496324d6b7ae1662e82e76c
Chad <keithchad10@outlook.com>
2020-12-30 10:51:34 +0300

    room entity

--- app/src/main/java/com/chad/roommvvm/Constants/Constants.java
+++ app/src/main/java/com/chad/roommvvm/Constants/Constants.java
@@ -3,5 +3,8 @@ package com.chad.roommvvm.Constants;
 public class Constants {
 
     public static final String TABLE_NAME = "note_table";
-    public static final String PRIORITY = "PRIORITY";
+    public static final String PRIORITY = "priority";
+    public static final String TITLE = "title";
+    public static final String DESCRIPTION = "description";
+
 }
--- app/src/main/java/com/chad/roommvvm/Model/Note.java
+++ app/src/main/java/com/chad/roommvvm/Model/Note.java
@@ -12,6 +12,50 @@ public class Note {
     @PrimaryKey(autoGenerate = true)
     private int id;
 
-    @ColumnInfo(name = )
+    @ColumnInfo(name = Constants.TITLE)
+    private String title;
+
+    @ColumnInfo(name = Constants.DESCRIPTION)
+    private String description;
+
+    @ColumnInfo(name = Constants.PRIORITY)
     private int priority;
+
+    public Note(String title, String description, int priority) {
+        this.title = title;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getPriority() {
+        return priority;
+    }
+
+    public void setPriority(int priority) {
+        this.priority = priority;
+    }
 }
